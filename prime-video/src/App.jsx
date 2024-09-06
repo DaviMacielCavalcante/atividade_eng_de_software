830eeace56b43800e7b3e7bc2e6bf07d22888f32
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2024-09-05 21:54:22 -0300

    :sparkles: feat: Criação do componente Home

--- prime-video/src/App.jsx
+++ prime-video/src/App.jsx
@@ -1,55 +1,8 @@
-import {useState} from "react"
-import styles from './App.module.css';
+import Home from './components/home/home'
 
 function App() {
-
-  const [login, setLogin] = useState("");
-  const [pwd, setPwd] = useState("");
-  const [msg, setMsg] = useState("");
-  const [valid, setValid] = useState(false);
-
-  const loginHandler = (e) => {
-    setLogin(e.target.value);
-  }
-
-  const pwdHandler = (e) => {
-    setPwd(e.target.value);
-  }
-
-  const validateCredentials = () => {
-    const user = "admin";
-    const senha = "12345";
-
-    if (login === user && pwd === senha) {
-      setValid(true);
-      setMsg("");
-    } else {
-      setMsg("Acesso negado!");
-    }
-  }
-
   return (
-    <div className={styles.container}>
-      <div>
-        <h1>Prime video</h1>
-        <p>login: </p>
-        <input 
-          type="text"
-          onChange={loginHandler}
-          value={login}
-          placeholder="informeu seu usuário"/>
-        <p>senha: </p>
-        <input 
-          type="text"
-          onChange={pwdHandler}
-          value={pwd}
-          placeholder="digite sua senha"/>
-        <div>
-          <button onClick={validateCredentials}>Login</button>
-          {validateCredentials && <p>{msg}</p>}
-        </div>
-      </div>
-    </div>
+    <Home></Home>
   );
 }
 
