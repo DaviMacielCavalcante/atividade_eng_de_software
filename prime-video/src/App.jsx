74e8110d749a9431f728d6b372501dfaac661fa0
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2024-09-14 19:00:40 -0300

    Revert "Merge branch 'dev' into selecao_de_conteudo"
    This reverts commit 6d58d5a47ebaa1a2abe702081f28d6b3d8b3f704, reversing
    changes made to 2f8995d898ed2e6b65854700cecf201473e094e4.

--- prime-video/src/App.jsx
+++ prime-video/src/App.jsx
@@ -1,9 +1,6 @@
 import { useState } from 'react';
 import Home from './components/home/home';
 import SelecaoConteudo from './components/selecaoConteudo/selecaoConteudo'; 
-import EscolherPerfil from './components/escolherPerfil/escolherPefil';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';   // Default import (no curly braces)
-
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false); 
@@ -12,13 +9,13 @@ function App() {
   };
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />    {/* Using Home as a component */}
-        <Route path="/escolherPerfil" element={<EscolherPerfil />} />
-        <Route path="/selecaoConteudo" element={<SelecaoConteudo/>} />   {/* Using MainMenu as a component */}
-      </Routes>
-    </Router>
+    <div>
+      {isAuthenticated ? (
+        <SelecaoConteudo /> 
+      ) : (
+        <Home onLoginSuccess={handleLoginSuccess} /> 
+      )}
+    </div>
   );
 }
 
